d26496b14c6b5d13a1c012707d7be6f090c9d5cc
tamanna1312 <108005745+tamanna1312@users.noreply.github.com>
2024-06-06 16:04:01 +0200

    Update streamlitwebpage.py

--- streamlitwebpage.py
+++ streamlitwebpage.py
@@ -6,7 +6,7 @@
 import streamlit as st
 import pandas as pd
 
-st.title('Geochemical Data Rock Label Predictor')
+st.title('TAS Rock Classifier')
 
 @st.cache(allow_output_mutation=True)
 def load_results():
@@ -17,10 +17,10 @@ def load_results():
 results = load_results()
 
 # Button to show results
-if st.button('Show Predictions'):
+if st.button('Upload Data'):
     # Display the DataFrame with the inverse-transformed features and predictions
     st.write("Data with Predicted Rock Types")
     st.dataframe(results)
-else:
-    st.write("Click the button to display the predictions.")
+# else:
+#     st.write("Click the button to display the predictions.")
 
