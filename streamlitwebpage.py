0f8de7742d807ef258720c5954ca007d153a78ca
tamanna1312 <108005745+tamanna1312@users.noreply.github.com>
2024-09-30 09:59:15 +0200

    Update streamlitwebpage.py

--- streamlitwebpage.py
+++ streamlitwebpage.py
@@ -5,14 +5,27 @@
 
 import streamlit as st
 import pandas as pd
+import numpy as np
+from tensorflow.keras.models import load_model
+from sklearn.preprocessing import StandardScaler
+import io
+
 
 st.title('TAS Rock Classifier')
 
-@st.cache(allow_output_mutation=True)
-def load_results():
-    # Load the precomputed predictions
-    results = pd.read_csv('predicted_results.csv')
-    return results
+def load_model_for_case(case):
+    if case == 'Case 1 - All Oxides':
+        return load_model('model_case_1.h5')
+    elif case == 'Case 2 - No SiO2':
+        return load_model('model_case_2.h5')
+    elif case == 'Case 3 - No Alkali Oxides':
+        return load_model('model_case_3.h5')
+
+# @st.cache(allow_output_mutation=True)
+# def load_results():
+#     # Load the precomputed predictions
+#     results = pd.read_csv('predicted_results.csv')
+#     return results
 
 results = load_results()
 
