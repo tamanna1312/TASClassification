48d76839ae34202fed6ac800102225dc555a9459
tamanna1312 <108005745+tamanna1312@users.noreply.github.com>
2024-10-14 10:37:15 +0200

    Update streamlitwebpage.py

--- streamlitwebpage.py
+++ streamlitwebpage.py
@@ -84,7 +84,7 @@ def TAS(ax,fontsize=8):
         '3: Trachyandesite', 
 ]
 
-    ax.text(74, 15, '\n'.join(full_field_names), 
+    ax.text(70, 14, '\n'.join(full_field_names), 
         transform=ax.transAxes, 
         fontsize=10, 
         verticalalignment='top', 
