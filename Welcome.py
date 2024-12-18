a5491160139c469512f63c6d5a0be1af6948d764
tamanna1312 <108005745+tamanna1312@users.noreply.github.com>
2024-12-18 18:10:51 +0100

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -1,5 +1,17 @@
 import streamlit as st
 
 st.header('Welcome')
+st.title("""Hello, welcome to the boron world""")
+
+st.header('Here are four pages from left Main menu', divider='rainbow')
+
+
+st.subheader(':red[(1) Import data:] upload datafiles from Neptune_Plus and laser')
+
+st.subheader(':green[(2) Data reduction:] choose standard for intra-sequence instrumental correction')
+
+st.subheader(':blue[(3) Data visualization and download:] check results and download files')
+
+st.subheader(':violet[(4) Documention:] Github code and detailed explainations')
 
 st.session_state.all_data = None
