8bfc01bc538a5bc103df54a1ba6998fd59c8a418
tamanna1312 <108005745+tamanna1312@users.noreply.github.com>
2024-12-21 22:36:37 +0100

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -1,17 +1,12 @@
 import streamlit as st
 
 # st.header('Welcome')
-st.header('Classfication of volcanic rocks using ML')
-# st.header('Data Upload:')
-# st.write('Users can upload their geochemical dataset in CSV format. The app validates the uploaded file to ensure compatibility with the selected case.')
-# st.header('Automatic Column Verification:')
-# st.write('The app checks whether the uploaded data contains all the required element oxides for the selected case (e.g., all oxides for Case 1, excluding SiO₂ for Case 2, or excluding alkali oxides for Case 3).')
-# st.write('Missing or incorrect columns will prompt the user to correct the dataset.')
-# st.write('The data is automatically rearranged to match the column order of the model’s training data.')
-# st.header('Predicted Rock Type:')
-# st.write('Once the data passes validation, the app uses the trained ML model to classify the rock types. The predicted rock type is displayed in a new column alongside the original dataset in a table format.')
-# st.header('TAS Plot Visualization:')
-# st.write('The predicted rock types are simultaneously plotted on the TAS diagram.')
+st.header('Welcome')
+st.write('The app contains 2 sections:')
+st.write(':purple[Classify Volcanic Rocks using ML model]'])
+Leverage a powerful machine learning model to classify volcanic rocks based on major and minor oxide data. The model predicts rock types from real-world and synthetic data, helping you understand complex rock compositions with ease.
+Generate Fake Data from Real-World Samples
+Use real-world data to generate synthetic datasets that mimic geochemical characteristics. This feature allows you to create "fake" data, ideal for testing or simulating various geological scenarios.
 
 
 
