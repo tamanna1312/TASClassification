253e34e17f390cca2e8ef5428cb501a15a22e26e
tamanna1312 <108005745+tamanna1312@users.noreply.github.com>
2024-12-21 09:20:56 +0100

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -1,17 +1,6 @@
 import streamlit as st
 
-st.header('Welcome')
-# st.title("""Hello, welcome to the boron world""")
-
-# st.header('Here are four pages from left Main menu', divider='rainbow')
-
-
-# st.subheader(':red[(1) Import data:] upload datafiles from Neptune_Plus and laser')
-
-# st.subheader(':green[(2) Data reduction:] choose standard for intra-sequence instrumental correction')
-
-# st.subheader(':blue[(3) Data visualization and download:] check results and download files')
-
-# st.subheader(':violet[(4) Documention:] Github code and detailed explainations')
-
+# st.header('Welcome')
+st.header('Welcome to the Geochemical Data Visualizer)
+st.write('An application designed for classification of volcanic rocks using ML.')
 st.session_state.all_data = None
