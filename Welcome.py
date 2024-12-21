f6c8b0239cb9e650d8649bf3d24f38b4c6ca703c
tamanna1312 <108005745+tamanna1312@users.noreply.github.com>
2024-12-21 10:10:08 +0100

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
 # st.header('Welcome')
-st.header('Classfication of Volcanic rocks using ML')
-st.write('An application designed for classification of volcanic rocks using ML.')
+st.header('Classfication of volcanic rocks using ML')
+
+# st.write('An application designed for classification of volcanic rocks using ML.')
+
 st.session_state.all_data = None
