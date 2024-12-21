682f369cb0245d8ffbef3fd227237a98ad39d7d5
tamanna1312 <108005745+tamanna1312@users.noreply.github.com>
2024-12-21 22:37:44 +0100

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -3,7 +3,7 @@ import streamlit as st
 # st.header('Welcome')
 st.header('Welcome')
 st.write('The app contains 2 sections:')
-st.write(':purple[Classify Volcanic Rocks using ML model'])
+st.write(':purple[Classify Volcanic Rocks using ML model]')
 Leverage a powerful machine learning model to classify volcanic rocks based on major and minor oxide data. The model predicts rock types from real-world and synthetic data, helping you understand complex rock compositions with ease.
 Generate Fake Data from Real-World Samples
 Use real-world data to generate synthetic datasets that mimic geochemical characteristics. This feature allows you to create "fake" data, ideal for testing or simulating various geological scenarios.
