c68c41ec4ebf67604fa1ada8e30a58e8645b8c02
tamanna1312 <108005745+tamanna1312@users.noreply.github.com>
2024-12-21 22:01:17 +0100

    Update 3_Documentation.py

--- pages/3_Documentation.py
+++ pages/3_Documentation.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # st.title('Documentation for Creating Fake Data')
 st.header('Classfication of volcanic rocks using ML')
-st.header('Data Upload:')
+st.subheader('Data Upload:')
 st.write('Users can upload their geochemical dataset in CSV format. The app validates the uploaded file to ensure compatibility with the selected case.')
 st.header('Automatic Column Verification:')
 st.write('The app checks whether the uploaded data contains all the required element oxides for the selected case (e.g., all oxides for Case 1, excluding SiO₂ for Case 2, or excluding alkali oxides for Case 3). Missing or incorrect columns will prompt the user to correct the dataset. The data is automatically rearranged to match the column order of the model’s training data.')
