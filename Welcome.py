b0d2d33fdac0a6bde4d5b397fd083cd1dacb2999
tamanna1312 <108005745+tamanna1312@users.noreply.github.com>
2024-12-21 09:21:14 +0100

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 # st.header('Welcome')
-st.header('Welcome to the Geochemical Data Visualizer)
+st.header('Welcome to the Geochemical Data Visualizer')
 st.write('An application designed for classification of volcanic rocks using ML.')
 st.session_state.all_data = None
