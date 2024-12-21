cb38ae43ffed986128edf1bd41c7661c9424c553
tamanna1312 <108005745+tamanna1312@users.noreply.github.com>
2024-12-21 22:45:46 +0100

    Update Welcome.py
    x

--- Welcome.py
+++ Welcome.py
@@ -5,9 +5,9 @@ st.header('Welcome')
 st.subheader('The application contains 2 sections:')
 # st.header(':blue[Using TAS Classifier]')
 st.subheader(':blue[Classify Volcanic Rocks using ML model]')
-st.write('Based on major and minor oxide data.')
+st.write('-> Based on major and minor oxide data.')
 st.subheader(':blue[Generate Fake Data from Real-World Data]')
-st.write('Use real-world data to generate fake datasets that mimic geochemical characteristics.')
+st.write('-> Use real-world data to generate fake datasets that tries to reflect the geochemical relationships within the oxides.')
 
 
 # st.write('An application designed for classification of volcanic rocks using ML.')
