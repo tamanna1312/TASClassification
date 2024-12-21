a138432c32af23d2a79fb6e017bb020abac2b982
tamanna1312 <108005745+tamanna1312@users.noreply.github.com>
2024-12-21 22:43:50 +0100

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -2,13 +2,12 @@ import streamlit as st
 
 # st.header('Welcome')
 st.header('Welcome')
-st.subheader('The app contains 2 sections:')
+st.subheader('The application contains 2 sections:')
 # st.header(':blue[Using TAS Classifier]')
 st.subheader(':blue[Classify Volcanic Rocks using ML model]')
-# Leverage a powerful machine learning model to classify volcanic rocks based on major and minor oxide data. The model predicts rock types from real-world and synthetic data, helping you understand complex rock compositions with ease.
-# Generate Fake Data from Real-World Samples
-# Use real-world data to generate synthetic datasets that mimic geochemical characteristics. This feature allows you to create "fake" data, ideal for testing or simulating various geological scenarios.
-
+st.write('Based on major and minor oxide data.')
+st.subheader(':blue[Generate Fake Data from Real-World Data]')
+st.write('Use real-world data to generate fake datasets that mimic geochemical characteristics.')
 
 
 # st.write('An application designed for classification of volcanic rocks using ML.')
