a1567aaa4c94dfd2425a224e43271b150ae2fbdf
tamanna1312 <108005745+tamanna1312@users.noreply.github.com>
2024-12-27 19:14:18 +0100

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -1,13 +1,5 @@
 import streamlit as st
 
-# st.header('Welcome')
-st.header('Welcome')
-st.subheader('The application contains 2 sections:')
-# st.header(':blue[Using TAS Classifier]')
-st.subheader(':blue[Classify Volcanic Rocks using ML model]')
-st.write('-> Based on major and minor oxides.')
-st.subheader(':blue[Generate Fake Data from Real-World Data]')
-st.write('-> Use real-world data to generate fake data that tries to reflect the geochemical relationships within the oxides.')
 
 def add_logo():
     st.markdown(
@@ -34,6 +26,16 @@ def add_logo():
     )
 
 add_logo()
+
+st.header('Welcome')
+st.subheader('The application contains 2 sections:')
+# st.header(':blue[Using TAS Classifier]')
+st.subheader(':blue[Classify Volcanic Rocks using ML model]')
+st.write('-> Based on major and minor oxides.')
+st.subheader(':blue[Generate Fake Data from Real-World Data]')
+st.write('-> Use real-world data to generate fake data that tries to reflect the geochemical relationships within the oxides.')
+
+
 col1, col2 = st.columns([3, 1])  
 
 with col1:
