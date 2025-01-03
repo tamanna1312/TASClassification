1441787b821001c7e5588818ef6d55ca326ff506
tamanna1312 <108005745+tamanna1312@users.noreply.github.com>
2025-01-03 22:42:55 +0100

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -18,6 +18,22 @@ def add_logo():
     )
 
 add_logo()
+st.markdown(
+    """
+    <style>
+        [data-testid=stSidebar] [data-testid=stImage]{
+            text-align: center;
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            width: 100%;
+        }
+    </style>
+    """, unsafe_allow_html=True
+)
+
+st.sidebar.image(
+    'images/Goethe-Logo.gif')
 
 
 
@@ -30,17 +46,17 @@ st.subheader(':blue[Generate Fake Data from Real-World Data]')
 st.write('-> Use real-world data to generate fake data that tries to reflect the geochemical relationships within the oxides.')
 
 
-col1, col2 = st.columns([3, 1])  
+# col1, col2 = st.columns([3, 1])  
 
-with col1:
-    pass
+# with col1:
+#     pass
 
-with col2:
-    # Add some spacing to move the image further right
-    st.markdown("<div style='padding-left: 50px;'></div>", unsafe_allow_html=True)
-    # Display the image on the right side with a smaller size
-    st.image('Goethe-Logo.gif', width=200)
-# st.image('Goethe-Logo.gif', use_column_width=False)
-# st.write('An application designed for classification of volcanic rocks using ML.')
+# with col2:
+#     # Add some spacing to move the image further right
+#     st.markdown("<div style='padding-left: 50px;'></div>", unsafe_allow_html=True)
+#     # Display the image on the right side with a smaller size
+#     st.image('Goethe-Logo.gif', width=200)
+# # st.image('Goethe-Logo.gif', use_column_width=False)
+# # st.write('An application designed for classification of volcanic rocks using ML.')
 
-st.session_state.all_data = None
+# st.session_state.all_data = None
