af92f7881b0ab637d4e4103942277b651c615c03
tamanna1312 <108005745+tamanna1312@users.noreply.github.com>
2025-01-04 19:12:04 +0100

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -6,7 +6,7 @@ def add_logo():
         """
         <style>
             [data-testid="stSidebarNav"] {
-                background-image: url('https://raw.githubusercontent.com/tamanna1312/TASClassification/main/IMG_3512.gif');
+                background-image: url('https://raw.githubusercontent.com/tamanna1312/TASClassification/main/IMG_3514.jpg');
                 background-repeat: no-repeat;
                 background-size: 150px 150px; /* Set explicit width and height */
                 background-position: 10px 10px; /* Position it in the top left */
