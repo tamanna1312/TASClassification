1dfd98748ae5a31af9aecdbd4d2f50c87b5cc3fb
tamanna1312 <108005745+tamanna1312@users.noreply.github.com>
2025-01-05 21:22:19 +0100

    Update Welcome.py
    x

--- Welcome.py
+++ Welcome.py
@@ -45,25 +45,3 @@ st.markdown("""
 ### :blue[Generate Fake Data from Real-World Data]
 - Use real-world data to generate fake data that tries to reflect the geochemical relationships within the oxides.
 """)
-
-# st.header('Welcome')
-# st.subheader('The application contains 2 sections:')
-# # st.header(':blue[Using TAS Classifier]')
-# st.subheader(':blue[Classify Volcanic Rocks using ML model]')
-# st.write('-> Based on major and minor oxides.')
-# st.subheader(':blue[Generate Fake Data from Real-World Data]')
-# st.write('-> Use real-world data to generate fake data that tries to reflect the geochemical relationships within the oxides.')
-# col1, col2 = st.columns([3, 1])  
-
-# with col1:
-#     pass
-
-# with col2:
-#     # Add some spacing to move the image further right
-#     st.markdown("<div style='padding-left: 50px;'></div>", unsafe_allow_html=True)
-#     # Display the image on the right side with a smaller size
-#     st.image('Goethe-Logo.gif', width=200)
-# # st.image('Goethe-Logo.gif', use_column_width=False)
-# # st.write('An application designed for classification of volcanic rocks using ML.')
-
-# st.session_state.all_data = None
