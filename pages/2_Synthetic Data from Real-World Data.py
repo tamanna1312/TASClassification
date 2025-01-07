7d2baa503de1d327b403ba579ea826bb79268896
tamanna1312 <108005745+tamanna1312@users.noreply.github.com>
2025-01-07 15:34:51 +0100

    Update 2_Synthetic Data from Real-World Data.py

--- pages/2_Synthetic Data from Real-World Data.py
+++ pages/2_Synthetic Data from Real-World Data.py
@@ -9,6 +9,42 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+def add_logo():
+    st.markdown(
+        """
+        <style>
+            [data-testid="stSidebarNav"] {
+                background-image: url('https://raw.githubusercontent.com/tamanna1312/TASClassification/main/Applogo.jpg');
+                background-repeat: no-repeat;
+                background-size: 150px 150px; /* Set explicit width and height */
+                background-position: 30px 10px; /* Position it in the top left */
+                # margin-top: 20px; /* Add space above */
+                padding-top: 170px; /* Add space below to separate from text */
+            }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+
+add_logo()
+
+st.markdown(
+    """
+    <style>
+        [data-testid=stSidebar] [data-testid=stImage]{
+            text-align: center;
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            width: 100%;
+        }
+    </style>
+    """, unsafe_allow_html=True
+)
+
+st.sidebar.image(
+    'Goethe-Logo.gif')
+
 # App title
 st.title("Synthetic Data Generator and Correlation Analysis")
 st.markdown("""
